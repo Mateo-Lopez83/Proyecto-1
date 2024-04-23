c4905ed3c431ac0932417d90b2d7abb1df35c69c
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2024-04-23 18:13:37 -0500

    funciona creo

--- intento.py
+++ intento.py
@@ -48,43 +48,14 @@ def llenar_matriz(matriz, diccionario, w1, w2, n):
 
 
 def crear_matriz(tamano):
-    # Crea una matriz nxn donde n es el tamaño del conjunto
+    # Matriz tiene que ser nxn y simetrica
     matriz = []
     for i in range(tamano):
         # Crea una nueva fila
         fila = [0] * tamano
         matriz.append(fila)
     return matriz
-'''def crearGrafo(lista, matriz, diccionario):
-    grafo = {}
 
-    # Primero, construir el diccionario de grafo con listas vacías para cada elemento fundamental
-    for elemento in lista:
-        if tuple(elemento) not in grafo:
-            grafo[tuple(elemento)] = []
-
-    # Buscar conexiones entre elementos con átomos iguales
-    for idx1, elemento1 in enumerate(lista):
-        atomo1, atomo2 = elemento1
-
-        # Buscar en otros elementos
-        for idx2, elemento2 in enumerate(lista):
-            if idx1 != idx2:  # Evitar comparar el elemento con sí mismo
-                atomo3, atomo4 = elemento2
-
-                # Comparar cada átomo del primer elemento con cada átomo del segundo elemento
-                for a1, a2 in [(atomo1, atomo3), (atomo1, atomo4), (atomo2, atomo3), (atomo2, atomo4)]:
-                    if a1 == a2:  # Si hay un átomo igual en ambos elementos
-                        # Buscar las posiciones en la matriz a través del diccionario
-                        pos1 = diccionario.get(a1)
-                        pos2 = diccionario.get(a2)
-
-                        if pos1 and pos2:
-                            # Agregar conexión al grafo, donde la llave es la tupla del elemento y el valor es un diccionario
-                            conexion = {tuple(elemento2): matriz[pos1][pos2]}
-                            grafo[tuple(elemento1)].append(conexion)
-
-    return grafo'''
 def crearGrafo(lista, matriz, caminos,indice_a_numero, numero_a_indice):
     vertices = []
     lista_ady = [[]]*(2*len(lista))
@@ -111,58 +82,144 @@ def crearGrafo(lista, matriz, caminos,indice_a_numero, numero_a_indice):
                 
             
         
-        
+def impares(graph):
+    odd_vertex = None
+    for node, neighbors in graph.items():
+        if odd_vertex is None:
+            odd_vertex = node
+        if len(neighbors) % 2 == 1:
+            odd_vertex = node
+            break
+    return odd_vertex
+
+
+def hay_dfs(v, u, graph):
+    if len(graph[v]) > 0:
+        visited = set()
+        cola = []
+        cola.append(v)
+        visited.add(v)
+        while len(cola)>0:
+            current_node = cola.pop(0)
+            for next_node in graph[current_node]:
+                if next_node == u:
+                    return True
+                if next_node not in visited:
+                    cola.append(next_node)
+                    visited.add(next_node)
+        return False
+    else:
+        return True
+
+
+def construir(parejas):
+    graph = {}
+    for elemento in parejas:
+        a = elemento[0]
+        b = elemento[1]
+        if a not in graph.keys():
+            graph[a] = []
+        graph[a].append(b)
+        if b not in graph.keys():
+            graph[b] = []
+        graph[b].append(a)
+    return graph
+
+
+def solve(parejas):
+    graph = construir(parejas)
+    num = len(parejas)
+    inicial = impares(graph)
+    path = []
+    for contador in range(num):
+        neighbors = graph[inicial]
+        for u in neighbors:
+            graph[inicial].remove(u)
+            graph[u].remove(inicial)
+            if hay_dfs(inicial, u, graph):
+                path.append((inicial, u))
+                inicial = u
+                break
+            else:
+                graph[inicial].append(u)
+                graph[u].append(inicial)
+    if len(path)!= len(parejas):
+        return None
+    return path
                 
         
     
 def main():
-  n, w1, w2 = 3, 100, 200
-  lista=[(1, 2),(1, 3),(1, 37),(1, 5),(1, 30),(3, 37),(5,30)]
-  diccionario  ={}
-  diccionario_inv = {}
-  contador = 0
-  atomos_libres = set()
-  for comp in lista:
-    atomos_libres.add(comp[0])
-    atomos_libres.add(-comp[0])
-    atomos_libres.add(comp[1])
-    atomos_libres.add(-comp[1])
-    if comp[0] not in diccionario.values():
-        diccionario[contador] = comp[0]
-        diccionario_inv[comp[0]] = contador
-        contador+=1
-    if comp[1] not in diccionario.values():
-        diccionario[contador] = comp[1]
-        diccionario_inv[comp[1]] = contador
-        contador+=1
-    if -comp[0] not in diccionario.values():
-        diccionario[contador] = -comp[0]
-        diccionario_inv[-comp[0]] = contador
-        contador+=1
-    if -comp[1] not in diccionario.values():
-        diccionario[contador] = -comp[1]
-        diccionario_inv[-comp[1]] = contador
-        contador+=1
+    n, w1, w2 = 3, 4,5
+    lista=[(1, 2),(1, 3),(1, 37),(1, 5),(1, 30),(3, 37),(5,30)]
+    #lista = [(1,3),(-6,3),(1,7)]
+    #lista = [(1,2),(2,3),(4,4)]
+    diccionario  ={}
+    diccionario_inv = {}
+    contador = 0
+    atomos_libres = set()
+    for comp in lista:
+        atomos_libres.add(comp[0])
+        atomos_libres.add(-comp[0])
+        atomos_libres.add(comp[1])
+        atomos_libres.add(-comp[1])
+        if comp[0] not in diccionario.values():
+            diccionario[contador] = comp[0]
+            diccionario_inv[comp[0]] = contador
+            contador+=1
+        if comp[1] not in diccionario.values():
+            diccionario[contador] = comp[1]
+            diccionario_inv[comp[1]] = contador
+            contador+=1
+        if -comp[0] not in diccionario.values():
+            diccionario[contador] = -comp[0]
+            diccionario_inv[-comp[0]] = contador
+            contador+=1
+        if -comp[1] not in diccionario.values():
+            diccionario[contador] = -comp[1]
+            diccionario_inv[-comp[1]] = contador
+            contador+=1
 
-  matriz = crear_matriz(len(atomos_libres))
-  matriz = llenar_matriz(matriz,diccionario, w1,w2,len(atomos_libres))
-  #print(matriz)
-  '''
-  print(matriz[diccionario_inv[1]][diccionario_inv[-1]])
-  print(matriz[diccionario_inv[30]][diccionario_inv[-37]])
-  print(matriz[diccionario_inv[5]][diccionario_inv[-5]])
-  print(matriz[diccionario_inv[-1]][diccionario_inv[-3]])
-  print(matriz[diccionario_inv[30]][diccionario_inv[30]])
-  print(matriz[diccionario_inv[2]][diccionario_inv[-2]])'''
-  distancias, caminos = FloydWarshall(matriz, len(matriz),diccionario)
-  '''print(distancias[diccionario_inv[1]][diccionario_inv[-1]])
-  print(distancias[diccionario_inv[30]][diccionario_inv[-37]])
-  print(distancias[diccionario_inv[5]][diccionario_inv[-2]])
-  print(distancias[diccionario_inv[-1]][diccionario_inv[-3]])
-  print(distancias[diccionario_inv[30]][diccionario_inv[30]])
-  print(distancias[diccionario_inv[2]][diccionario_inv[-2]])'''
-  grafo = crearGrafo(lista, matriz, caminos,diccionario, diccionario_inv)
-  #print(grafo)
-  
+    matriz = crear_matriz(len(atomos_libres))
+    matriz = llenar_matriz(matriz,diccionario, w1,w2,len(atomos_libres))
+
+    #print(matriz)
+    '''
+    print(matriz[diccionario_inv[1]][diccionario_inv[-1]])
+    print(matriz[diccionario_inv[30]][diccionario_inv[-37]])
+    print(matriz[diccionario_inv[5]][diccionario_inv[-5]])
+    print(matriz[diccionario_inv[-1]][diccionario_inv[-3]])
+    print(matriz[diccionario_inv[30]][diccionario_inv[30]])
+    print(matriz[diccionario_inv[2]][diccionario_inv[-2]])'''
+    distancias, caminos = FloydWarshall(matriz, len(matriz),diccionario)
+    resp = ""
+    LTPs = 0
+    euler = solve(lista)
+    if euler is not None:
+        for indice in range(len(euler)):
+            pareja = euler[indice]
+            conectado = int(pareja[1])
+            
+            costo = distancias[diccionario_inv[conectado]][diccionario_inv[-conectado]]
+            if indice!= len(euler)-1:
+                LTPs += costo
+            camino  = caminos[diccionario_inv[conectado]][diccionario_inv[-conectado]]
+            camino.pop(0)
+            resp += str(pareja)
+                
+            if indice!= len(euler)-1:
+                for numero in camino:
+                    
+                    resp+=","+str(numero)
+                
+            
+        resp+=" "+str(LTPs)
+        
+    else:
+        resp = "NO HAY CAMINO"
+    
+    print(resp)
       
 main()
+
+
